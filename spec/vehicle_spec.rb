e3a0e84dd6f7699f4180295e1da891808c3ece8b
NoahZinter <noah.zinter@gmail.com>
2021-04-22 10:03:08 -0600

    Test add passenger

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -39,4 +39,18 @@ describe Vehicle do
       expect(vehicle.passengers).to eq []
     end
   end
+
+  describe '#add_passenger' do
+    it 'adds passengers to @passengers' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+      expect(vehicle.passengers).to eq ([charlie, jude, taylor])
+    end
+  end
 end
